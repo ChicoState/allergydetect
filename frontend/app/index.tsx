3ff392059fe3a2080661d8d2792ef6887d38c2f4
Kenai Toledo <ktoledo@csuchico.edu>
2026-02-26 10:30:00 -0800

    Update index screen

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,9 +1,26 @@
-import { Text, View,  StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, Button } from 'react-native';
+import { useState } from 'react';
 
 export default function Index() {
+  const [ingredients, setIngredients] = useState<string[]>([]);
+
+  async function handlePress() {
+    const data = await getIngredients('894700010021');
+    setIngredients(data);
+    console.log(data);
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Home screen</Text>
+
+      <Button title="Test UPC" onPress={handlePress} />
+
+      {ingredients.map((item, index) => (
+        <Text key={index} style={styles.text}>
+          {item}
+        </Text>
+      ))}
     </View>
   );
 }
@@ -21,11 +38,10 @@ const styles = StyleSheet.create({
 });
 
 async function getIngredients(upc: string): Promise<string[]> {
-  const response = await fetch(`https://api.upcitemdb.com/prod/trial/lookup?upc=${upc}`);
-  const data = await response.json();
-  const ingredients = data.items[0].description;
-  //console.log(ingredients.split(', '));
-  return ingredients.split(', ');
+const response = await fetch(`http://localhost:3000/ingredients/${upc}`);
+const data = await response.json();
+return data.ingredients;
 }
+
 
 //getIngredients('894700010021');
